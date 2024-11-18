1fe462791867f662e9d5eb607d5a27fc7a3be664
prokh <prokhozheva.elena@gmail.com>
2024-11-19 01:16:40 +0400

    Scos

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,87 +1,80 @@
-﻿class Calculator
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+public delegate double MyFunctionDelegate(double x);
+public class Area
 {
-    public static double DoOperation(double num1, double num2, string op)
+    public static double TrapRule(MyFunctionDelegate function, double cleanlimleft, double cleanlimright, int cleanstep)
     {
-        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-
-        // Use a switch statement to do the math.
-        switch (op)
+        // a, b - пределы интегрирования
+        // n - количество разбиений
+        double h = (cleanlimright - cleanlimleft) / cleanstep;
+        double sum = 0.5 * (function(cleanlimleft) + function(cleanlimright));
+        for (int i = 1; i < cleanstep                                      ; i++)
         {
-            case "a":
-                result = num1 + num2;
-                break;
-            case "s":
-                result = num1 - num2;
-                break;
-            case "m":
-                result = num1 * num2;
-                break;
-            case "d":
-                // Ask the user to enter a non-zero divisor.
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
-                break;
-            // Return text for an incorrect option entry.
-            default:
-                break;
+            double x = cleanlimleft + i * h;
+            sum += function(x);
         }
-        return result;
+        return h * sum;
     }
 }
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    
+    static double myFunction(double x)
+    {
+        return Math.Cos(x);
+    }
+    public static void Main(string[] args)
     {
         bool endApp = false;
         // Display title as the C# console calculator app.
-        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine("Find Scos in C#\r");
         Console.WriteLine("------------------------\n");
 
         while (!endApp)
         {
             // Declare variables and set to empty.
-            string numInput1 = "";
-            string numInput2 = "";
+            string limleft = "";
+            string limright = "";
+            string step = "";
             double result = 0;
 
             // Ask the user to type the first number.
-            Console.Write("Type a number, and then press Enter: ");
-            numInput1 = Console.ReadLine();
+            Console.Write("Type a left limit to calculate, and then press Enter: ");
+            limleft = Console.ReadLine();
 
-            double cleanNum1 = 0;
-            while (!double.TryParse(numInput1, out cleanNum1))
+            double cleanlimleft = 0;
+            while (!double.TryParse(limleft, out cleanlimleft))
             {
-                Console.Write("This is not valid input. Please enter an integer value: ");
-                numInput1 = Console.ReadLine();
+                Console.Write("This is not valid input. Please enter correct value: ");
+                limleft = Console.ReadLine();
             }
 
             // Ask the user to type the second number.
-            Console.Write("Type another number, and then press Enter: ");
-            numInput2 = Console.ReadLine();
+            Console.Write("Type a right limit to calculate, and then press Enter: ");
+            limright = Console.ReadLine();
 
-            double cleanNum2 = 0;
-            while (!double.TryParse(numInput2, out cleanNum2))
+            double cleanlimright = 0;
+            while (!double.TryParse(limright, out cleanlimright))
             {
-                Console.Write("This is not valid input. Please enter an integer value: ");
-                numInput2 = Console.ReadLine();
+                Console.Write("This is not valid input. Please enter correct value: ");
+                limright = Console.ReadLine();
+            }
+            // Ask the user to type the second number.
+            Console.Write("Type a step to calculate, and then press Enter: ");
+            step = Console.ReadLine();
+
+            int cleanstep = 0;
+            while (!int.TryParse(step, out cleanstep))
+            {
+                Console.Write("This is not valid input. Please enter correct value: ");
+                step = Console.ReadLine();
             }
 
-            // Ask the user to choose an operator.
-            Console.WriteLine("Choose an operator from the following list:");
-            Console.WriteLine("\ta - Add");
-            Console.WriteLine("\ts - Subtract");
-            Console.WriteLine("\tm - Multiply");
-            Console.WriteLine("\td - Divide");
-            Console.Write("Your option? ");
-
-            string op = Console.ReadLine();
 
             try
             {
-                result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                result = Area.TrapRule(myFunction, cleanlimleft, cleanlimright, cleanstep);
                 if (double.IsNaN(result))
                 {
                     Console.WriteLine("This operation will result in a mathematical error.\n");
@@ -104,3 +97,9 @@ class Program
         return;
     }
 }
+
+
+// Пример функции
+// Вычисление интеграла от 0 до Pi/2
+//double result = NumericalIntegration.TrapezoidalRule(myFunction, 0, Math.PI / 2, 1000);
+//Console.WriteLine(result);
